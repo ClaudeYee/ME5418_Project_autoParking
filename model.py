2dcf4ea0eb53e5f2f8ec46780f34196719b19d18
Nep <li_y@u.nus.edu>
2023-10-25 13:57:31 +0800

    Uploaded Network of pklot_world input.

--- model.py
+++ model.py
@@ -6,18 +6,18 @@ import torch.nn.functional as F
 class ActorNet(nn.Module):
     class obs_robot_world1(nn.Module):
 
-        def __init__(self, input_depth, output_depth = None):
+        def __init__(self, input_depth, output_depth=None):
             super(obs_robot_world2, self).__init__()
 
             if output_depth is None:
                 output_depth = input_depth * 16
 
             self.obs_robot_world_input = nn.Sequential(
-                nn.Conv2d(input_depth, output_depth, 5, stride=1, padding=2), # 60*60*32
+                nn.Conv2d(input_depth, output_depth, 5, stride=1, padding=2),  # 60*60*32
                 nn.ReLU(),
-                nn.Conv2d(output_depth, output_depth, 3, stride=1, padding=1), # 60*60*32
+                nn.Conv2d(output_depth, output_depth, 3, stride=1, padding=1),  # 60*60*32
                 nn.ReLU(),
-                nn.MaxPool2d(2), # 30*30*32
+                nn.MaxPool2d(2),  # 30*30*32
             )
 
         def forward(self, x):
@@ -27,17 +27,17 @@ class ActorNet(nn.Module):
 
         def __init__(self, input_depth):
             super(obs_robot_world2, self).__init__()
-            output_depth = input_depth*2
+            output_depth = input_depth * 2
 
             self.obs_robot_world_input = nn.Sequential(
-                nn.Conv2d(input_depth, output_depth, 3, stride=1, padding=1),   # 30*30*64
+                nn.Conv2d(input_depth, output_depth, 3, stride=1, padding=1),  # 30*30*64
                 nn.ReLU(),
-                nn.MaxPool2d(2),    # 15*15*64
-                nn.Conv2d(output_depth, output_depth * 2, 3, stride=2, padding=0),    # 7*7*128
+                nn.MaxPool2d(2),  # 15*15*64
+                nn.Conv2d(output_depth, output_depth * 2, 3, stride=2, padding=0),  # 7*7*128
                 nn.ReLU(),
                 nn.Conv2d(output_depth * 2, output_depth * 4, 3, stride=2, padding=1),  # 4*4*256
                 nn.ReLU(),
-                nn.MaxPool2d(2),    # 2*2*256
+                nn.MaxPool2d(2),  # 2*2*256
                 nn.Conv2d(output_depth * 4, output_depth * 8, 3, stride=1, padding=1),  # 2*2*512
                 nn.MaxPool2d(2),  # 512*1
             )
@@ -62,7 +62,7 @@ class ActorNet(nn.Module):
                     nn.MaxPool2d(2),  # 2*2*256
                     nn.Conv2d(output_depth * 4, output_depth * 8, 3, stride=1, padding=1),  # 2*2*512
                     nn.MaxPool2d(2),  # 1*1*512
-                    nn.Flatten(),   # 512*1
+                    nn.Flatten(),  # 512*1
                 )
 
             def forward(self, x):
@@ -81,7 +81,9 @@ class ActorNet(nn.Module):
                     nn.Conv2d(4, output_depth, 3, stride=1, padding=1),  # 5*5*16
                     nn.ReLU(),
                     nn.MaxPool2d(5),  # 1*1*16
-                    nn.Flatten(),   # 16*1
+                    nn.Flatten(),  # 16*1
+                    nn.Linear(16, 16),  # 16*1
+                    nn.ReLU(),
 
                 )
 
